5b25ae335e459c7420af7edf7f69304594aeb7ab
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2018-12-05 17:06:36 +0100

    randomized the hello message and added random values to be used at formatting

--- springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -15,12 +17,31 @@ public class HelloWebController {
 
 
     private final AtomicLong counter = new AtomicLong();
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
     @RequestMapping(value = "/web/greeting")
     public String greeting(Model model, @RequestParam(value = "name", defaultValue = "World") String name) {
 
+        Random rand = new Random();
+
+        int helloIndex = rand.nextInt(hellos.length);
+        int size = rand.nextInt();
+
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+
+        Color randColor = new Color(r, g, b);
+
+        model.addAttribute("hello", hellos[helloIndex]);
         model.addAttribute("name", " "+name);
         model.addAttribute("count", counter.incrementAndGet());
+
+
+
         return "greeting";
 
     }
