fe743e489428af047051c64572e3df8b3fa0c2cf
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2018-12-07 13:21:05 +0100

    wrote the shop class

---
+++ week-07/day-5/Webshop/src/main/java/com/greenfoxacademy/webshop/controllers/AddItems.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.webshop.controllers;
+
+public class AddItems {
+}
--- week-07/day-5/Webshop/src/main/java/com/greenfoxacademy/webshop/models/Shop.java
+++ week-07/day-5/Webshop/src/main/java/com/greenfoxacademy/webshop/models/Shop.java
@@ -1,4 +1,21 @@
 package com.greenfoxacademy.webshop.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop {
+
+    List<Item> ShopItems;
+
+    public List<Item> getShopItems() {
+        return ShopItems;
+    }
+
+    public Shop(List<Item> shopItems) {
+        ShopItems = shopItems;
+    }
+
+    public void Add (Item i){
+        ShopItems.add(i);
+    }
 }
