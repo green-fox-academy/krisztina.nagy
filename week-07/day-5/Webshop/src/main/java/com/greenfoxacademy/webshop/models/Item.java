af2889f6e7b09c863af9b4003b5c36d8c731d54f
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2018-12-07 13:21:39 +0100

    wrote the item class

--- week-07/day-5/Webshop/src/main/java/com/greenfoxacademy/webshop/models/Item.java
+++ week-07/day-5/Webshop/src/main/java/com/greenfoxacademy/webshop/models/Item.java
@@ -1,4 +1,48 @@
 package com.greenfoxacademy.webshop.models;
 
 public class Item {
+
+    String name;
+    String description;
+    Float price;
+    Integer quantityInStock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Item(String name, String description, Float price, Integer quantityInStock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+    }
 }
