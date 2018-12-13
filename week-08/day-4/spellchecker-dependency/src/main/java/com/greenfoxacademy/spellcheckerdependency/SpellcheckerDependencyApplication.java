6c56305deadaf74796bd6091025d07de77e0c20a
Krisztina Nagy <the.nagy.kriszta@gmail.com>
2018-12-13 12:25:54 +0100

    initial commit, added project dir and created project

---
+++ week-08/day-4/spellchecker-dependency/src/main/java/com/greenfoxacademy/spellcheckerdependency/SpellcheckerDependencyApplication.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.spellcheckerdependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellcheckerDependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpellcheckerDependencyApplication.class, args);
+    }
+
+}
+
